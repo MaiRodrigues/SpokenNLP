e39f6314a3d9c0cc4f27e875767fd1f93f17da78
Luciano Augusto Campagnoli da Silva <luti4497@gmail.com>
2024-02-29 10:16:25 -0300

    fix: Corrige versão com leituras de caminho

--- traducao_do_dataset_v1_azure.py
+++ traducao_do_dataset_v1_azure.py
@@ -1,8 +1,7 @@
 
 
-import requests, uuid, os
-from dotenv import load_dotenv
-
+# -*- coding: utf-8 -*-
+import requests, uuid, os, time
 
 # Add your key and endpoint
 key = os.environ["AZURE_TRANSLATION_API_KEY"]
@@ -12,8 +11,8 @@ endpoint = os.environ["AZURE_TRANSLATION_API_BASE_URL"]
 # required if you're using a multi-service or regional (not global) resource. It can be found in the Azure portal on the Keys and Endpoint page.
 location = os.environ["AZURE_TRANSLATION_API_RESOURE_LOCATION"]
 
-input_location = "segformer/en_disease_validation" # pasta com dataset original
-output_location = "segformer/output_azure_train" # pasta com as traduções
+input_location = os.path.join("segformer", "en_disease_validation") # pasta com dataset original
+output_location = os.path.join("segformer", "output_azure_validation") # pasta com as traduções
 
 params = {
     "api-version": "3.0",
@@ -28,8 +27,8 @@ headers = {
     "X-ClientTraceId": str(uuid.uuid4())
 }
 
-number_files: int = 0
-
+limit: int = 25000
+order: int = 0
 
 for root, dir, files in os.walk(input_location):
     for file in files:
@@ -37,21 +36,32 @@ for root, dir, files in os.walk(input_location):
             new_file = "{}_translated.txt".format(file.split('.')[0])
 
             # faz a leitura dos arquivos
-            with open(os.path.join(root, file), 'r') as f:
+            with open(os.path.join(root, file), 'r', encoding="utf-8") as f:
                 file_contents = f.read()
-                
+              
+            print("Comprimento: ")
+            print(len(file_contents))
+              
             body = [{
                 "text": file_contents
-            }]
+            }]    
 
             print("Oi")
             # traduz os arquivos
-            request = requests.post(endpoint + "/translate", params=params, headers=headers, json=body)
-            response = request.json()
-
-            print("Tchau")
-            number_files +=1
-            print("Arquivos traduzidos: ", number_files)
-            # salva os novos arquivos traduzidos em outra pasta
-            with open(os.path.join(output_location, new_file), 'w') as f:
-                f.write(api_response.text)
+            api_response = requests.post(endpoint + "/translate", params=params, headers=headers, json=body).json()
+            
+            if(isinstance(api_response, list)):
+                # salva os novos arquivos traduzidos em outra pasta
+                with open(os.path.join(output_location, new_file), 'w') as f:
+                    f.write(str(api_response[0]["translations"][0]["text"]))
+                    
+            else:
+                print("Resposta de erro:")
+                print(api_response)
+                
+            order += 1
+            
+            # Rate limit
+            if (order % 4 == 0):
+                time.sleep(10)
+                    
